8c755c2e37e83ba3da4cc16081a93bc4dff109ff
melicias <melicias1999@gmail.com>
2020-10-06 11:56:24 +0100

    atrgs in main prob working, only the basics

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 /**
 * @file main.c
 * @brief Description
-* @date 2018-1-1
-* @author name of author
+* @date 2020-10-6
+* @author Francisco Melicias - 2191727 & Diogo Francisco - 219----
 */
 
 #include <stdio.h>
@@ -12,16 +12,27 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <stdbool.h>
 
 #include "debug.h"
 #include "memory.h"
+#include "args.h"
+
 
 int main(int argc, char *argv[]){
-	/* Disable warnings */
-	(void)argc; (void)argv;
+	
+	struct gengetopt_args_info args;
 
-	/* Main code */
-	DEBUG("Main is empty -- add code here");
+	// gengetopt parser
+	if(cmdline_parser(argc, argv, &args))
+		ERROR(1, "[Error]: cmdline_parser execution.\n");
+
+	// all the option only must be used alone, so 1 is always from the name of file
+	if(argc > 2)
+		ERROR(2, "[Error]: All the option must to be used on their own.\n");
+
+	// gengetopt: release resources
+	//cmdline_parser_free(&args);
 
 
 	return 0;
